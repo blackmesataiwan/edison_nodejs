29db85725f6fb35f8ee3ee1b05894f6a4bb9696d
blackmesataiwan <san82718@gmail.com>
2017-03-21 18:51:50 +0800

    add smartobject function and fix some probram

--- Subscrib/main.js
+++ Subscrib/main.js
@@ -2,11 +2,11 @@ var QIoT = require('./QIoT');
 var m = require('mraa'); //require mraa
 console.log('MRAA Version: ' + m.getVersion()); //write the mraa version to the console
 
-var Sound = new m.Aio(0);
+//var Sound = new m.Aio(0);
 var Rotary = new m.Aio(1);
 var Piezo = new m.Aio(2);
 var Light = new m.Aio(3);
-//var Temperature = new m.Aio(0);  
+var Temperature = new m.Aio(0);  
 
 
 var Button = new m.Gpio(2); //setup digital read on pin 6
@@ -22,7 +22,12 @@ var Qclient = QIoT.qiotmqtt.start('./res/resourceinfo.json');
 	Receive data of QIoT Suite Lite.
 ***/
 
+//Setting Subscribe is use id <qiotmqtt.subscribeofid("ID", Qclient);>
+//It will return topic name
+
 var topic_LED = QIoT.qiotmqtt.subscribeofid("LED", Qclient);
+
+//It's Switch case of topic name to receive message
 
 Qclient.on('message', function(topic, message){
 	var data = JSON.parse(message.toString());
@@ -52,18 +57,23 @@ Qclient.on('message', function(topic, message){
 	Send sensor's data to QIoT Suite Lite by Resourcetype.
 ***/
 
+//It's use "resourcetypename" to sending data.
+//QIoT.qiotmqtt.type("resourcetypename", value, Qclient);
+
+
 function sensors(){
-	
 	//QIoT.qiotmqtt.type("Temperature",DHTSensor.read()[0],Qclient);
 	//QIoT.qiotmqtt.type("Humidity",DHTSensor.read()[1],Qclient);
-	//a = Temperature.read();
-	//QIoT.qiotmqtt.type("Temperature",1/(log(((1023-a)*10000/a)/10000)/3975+1/298.15)-273.15,Qclient); //anolog temperature
+	
+	a = Temperature.read();
+	QIoT.qiotmqtt.type("Temperature",(1/(Math.log(((1023-a)*10000/a)/10000)/3975+1/298.15)-273.15).toFixed(2),Qclient); //anolog temperature
+	
 	QIoT.qiotmqtt.type("Button",Button.read(),Qclient);
 	QIoT.qiotmqtt.type("Touch",Touch.read(),Qclient);
-	QIoT.qiotmqtt.type("Sound",parseInt(Sound.read()/1023*100, 10),Qclient);
+	//QIoT.qiotmqtt.type("Sound",parseInt(Sound.read()/1023*100, 10),Qclient);
 	QIoT.qiotmqtt.type("Rotary Angle",parseInt(Rotary.read()/1023*100, 10),Qclient);
 	QIoT.qiotmqtt.type("Light",Light.read(),Qclient);
-	QIoT.qiotmqtt.type("Piezo Vibration",Piezo.read(),Qclient);
+	QIoT.qiotmqtt.type("Piezo Vibration",Piezo.read()>100 ? 1:0,Qclient);
 
 	setTimeout(function() {
 		console.log("wating......");
--- smartobject/smartobject.js
+++ smartobject/smartobject.js
@@ -1,9 +1,11 @@
 var m = require('mraa'),
-    SmartObject = require('smartobject');
+    SmartObject = require('smartobject'),
+    QIoT = require('./QIoT');
 
 // Add our led blink driver to hal
 var sensor = new SmartObject({
-    
+
+    //Temperature: new m.Aio(0),
     Sound: new m.Aio(0),
     Rotary: new m.Aio(1),
     Piezo: new m.Aio(2),
@@ -12,15 +14,19 @@ var sensor = new SmartObject({
     Button: new m.Gpio(2),
     Touch: new m.Gpio(4),
 
+    LED: new m.Gpio(7),
+
 }, function () {
     var self = this;
 
     this.hal.Button.dir(m.DIR_IN);
-    this.hal.Touch.dir(m.DIR_IN);  // set up direction for the button gpio
+    this.hal.Touch.dir(m.DIR_IN);
+    this.hal.LED.dir(m.DIR_OUT);  // set up direction for the button gpio
 });
 
 // Analog Input (oid = 3202 or 'aIn')
-sensor.init('aIn', 0, {
+function initsensor (){
+    sensor.init('aIn', 0, {
     aInCurrValue: {                 // < rid = 5600, R, Float >
         read: function (cb) {
             var hal = this.parent.hal,
@@ -35,72 +41,179 @@ sensor.init('aIn', 0, {
     // resetMinMaxMeaValues: ,      // < rid = 5605,  E, Opaque >
     // appType: ,                   // < rid = 5750, RW, String >
     sensorType: 'Sound'                 // < rid = 5751,  R, String >
-});
+    });
 
-sensor.init('aIn', 1, {
-    aInCurrValue: {                 // < rid = 5600, R, Float >
+    sensor.init('aIn', 1, {
+        aInCurrValue: {                 // < rid = 5600, R, Float >
+            read: function (cb) {
+                var hal = this.parent.hal,
+                    value = parseInt(hal.Rotary.read()/1023*100, 10);
+                cb(null, value);
+            }
+        },
+        // minMeaValue: ,               // < rid = 5601,  R, Float >
+        // maxMeaValue: ,               // < rid = 5602,  R, Float >
+        minRangeValue: 0,             // < rid = 5603,  R, Float >
+        maxRangeValue: 100,             // < rid = 5604,  R, Float >
+        // resetMinMaxMeaValues: ,      // < rid = 5605,  E, Opaque >
+        // appType: ,                   // < rid = 5750, RW, String >
+        sensorType: 'Rotary'                 // < rid = 5751,  R, String >
+    });
+
+    sensor.init('aIn', 2, {
+        aInCurrValue: {                 // < rid = 5600, R, Float >
+            read: function (cb) {
+                var hal = this.parent.hal,
+                    value = hal.Piezo.read()>100 ? 1:0;
+                cb(null, value);
+            }
+        },
+        // minMeaValue: ,               // < rid = 5601,  R, Float >
+        // maxMeaValue: ,               // < rid = 5602,  R, Float >
+        minRangeValue: 0,             // < rid = 5603,  R, Float >
+        maxRangeValue: 100,             // < rid = 5604,  R, Float >
+        // resetMinMaxMeaValues: ,      // < rid = 5605,  E, Opaque >
+        // appType: ,                   // < rid = 5750, RW, String >
+        sensorType: "Piezo Vibration"                 // < rid = 5751,  R, String >
+    });
+
+    sensor.init('aIn', 3, {
+        aInCurrValue: {                 // < rid = 5600, R, Float >
+            read: function (cb) {
+                var hal = this.parent.hal,
+                    value = hal.Light.read();
+                cb(null, value);
+            }
+        },
+        // minMeaValue: ,               // < rid = 5601,  R, Float >
+        // maxMeaValue: ,               // < rid = 5602,  R, Float >
+        // minRangeValue: ,             // < rid = 5603,  R, Float >
+        // maxRangeValue: ,             // < rid = 5604,  R, Float >
+        // resetMinMaxMeaValues: ,      // < rid = 5605,  E, Opaque >
+        // appType: ,                   // < rid = 5750, RW, String >
+        sensorType: 'Light'                 // < rid = 5751,  R, String >
+    });
+
+    sensor.init('dIn', 0, {
+    dInState: {                     // < rid = 5500, R, Boolean >
         read: function (cb) {
             var hal = this.parent.hal,
-                value = parseInt(hal.Rotary.read()/1023*100, 10);
+                value = hal.Button.read();
+                cb(null, value);
+        }
+    },
+    // counter: ,                   // < rid = 5501,  R, Integer >
+    // dInPolarity: ,               // < rid = 5502, RW, Boolean >
+    // debouncePeriod: ,            // < rid = 5503, RW, Integer, ms >
+    // edgeSelection: ,             // < rid = 5504, RW, Integer { 1: fall, 2: rise, 3: both } >
+    // counterReset: ,              // < rid = 5505,  E, Opaque >
+    // appType: ,                   // < rid = 5750, RW, String >
+    sensorType: "Button"                 // < rid = 5751,  R, String >
+    });
+
+    sensor.init('dIn', 1, {
+    dInState: {                     // < rid = 5500, R, Boolean >
+        read: function (cb) {
+            var hal = this.parent.hal,
+                value = hal.Button.read();
+                cb(null, value);
+        }
+    },
+    // counter: ,                   // < rid = 5501,  R, Integer >
+    // dInPolarity: ,               // < rid = 5502, RW, Boolean >
+    // debouncePeriod: ,            // < rid = 5503, RW, Integer, ms >
+    // edgeSelection: ,             // < rid = 5504, RW, Integer { 1: fall, 2: rise, 3: both } >
+    // counterReset: ,              // < rid = 5505,  E, Opaque >
+    // appType: ,                   // < rid = 5750, RW, String >
+    sensorType: "Touch"                 // < rid = 5751,  R, String >
+    });
+
+    sensor.init('dOut', 0, {
+    dOutState: {                    // < rid = 5550, RW, Boolean >
+        read: function (cb) {
+            var hal = this.parent.hal,
+            value = hal.LED.read();
+            cb(null, value);
+        },
+        write: function (value, cb) {
+            var hal = this.parent.hal
+            hal.LED.write(value);
             cb(null, value);
         }
     },
-    // minMeaValue: ,               // < rid = 5601,  R, Float >
-    // maxMeaValue: ,               // < rid = 5602,  R, Float >
-    minRangeValue: 0,             // < rid = 5603,  R, Float >
-    maxRangeValue: 100,             // < rid = 5604,  R, Float >
-    // resetMinMaxMeaValues: ,      // < rid = 5605,  E, Opaque >
-    // appType: ,                   // < rid = 5750, RW, String >
-    sensorType: 'Rotary'                 // < rid = 5751,  R, String >
-});
-
-sensor.init('aIn', 2, {
-    aInCurrValue: {                 // < rid = 5600, R, Float >
+    // dOutpolarity: ,              // < rid = 5551, RW, Boolean { 0: normal, 1: reversed } >
+    // appType:                     // < rid = 5750, RW, String >
+    });
+/*
+    sensor.init('temperature', 0, {
+    sensorValue: {                  // < rid = 5700, R, Float >
         read: function (cb) {
             var hal = this.parent.hal,
-                value = hal.Piezo.read();
-            cb(null, value);
+                    value = hal.Temperature.read();
+                    value =(1/(Math.log(((1023-value)*10000/value)/10000)/3975+1/298.15)-273.15).toFixed(2);
+                cb(null, value);
         }
     },
-    // minMeaValue: ,               // < rid = 5601,  R, Float >
-    // maxMeaValue: ,               // < rid = 5602,  R, Float >
-    minRangeValue: 0,             // < rid = 5603,  R, Float >
-    maxRangeValue: 100,             // < rid = 5604,  R, Float >
-    // resetMinMaxMeaValues: ,      // < rid = 5605,  E, Opaque >
-    // appType: ,                   // < rid = 5750, RW, String >
-    sensorType: "Piezo Vibration"                 // < rid = 5751,  R, String >
+    units: "C",                     // < rid = 5701, R, String >
+    // minMeaValue: ,               // < rid = 5601, R, Float >
+    // maxMeaValue: ,               // < rid = 5602, R, Float >
+    // minRangeValue: ,             // < rid = 5603, R, Float >
+    // maxRangeValue: ,             // < rid = 5604, R, Float >
+    // resetMinMaxMeaValues:        // < rid = 5605, E, Opaque >
+    sensorType: "Temperature"
+    });
+*/
+}
+
+var Qclient = QIoT.qiotmqtt.start('./res/resourceinfo.json');
+
+var topic_LED = QIoT.qiotmqtt.subscribeofid("LED", Qclient);
+
+Qclient.on('message', function(topic, message){
+    var data = JSON.parse(message.toString());
+    switch (topic){
+        case topic_LED:
+            if (data.value == 1) {
+                sensor.write('dOut', 0, 'dOutState', 1);
+            }
+            else{
+                sensor.write('dOut', 0, 'dOutState', 0);
+            }
+            break;
+
+        default:
+                
+            break;
+    }
+    console.log(topic_LED);
+    console.log(data.value);
+    //LED.write(LED.read()?0:1);
+
 });
 
-sensor.init('aIn', 3, {
-    aInCurrValue: {                 // < rid = 5600, R, Float >
-        read: function (cb) {
-            var hal = this.parent.hal,
-                value = hal.Light.read();
-            cb(null, value);
-        }
-    },
-    // minMeaValue: ,               // < rid = 5601,  R, Float >
-    // maxMeaValue: ,               // < rid = 5602,  R, Float >
-    // minRangeValue: ,             // < rid = 5603,  R, Float >
-    // maxRangeValue: ,             // < rid = 5604,  R, Float >
-    // resetMinMaxMeaValues: ,      // < rid = 5605,  E, Opaque >
-    // appType: ,                   // < rid = 5750, RW, String >
-    sensorType: 'Light'                 // < rid = 5751,  R, String >
-});
+function senddata (oid,iid,rid){
+    var sensortype = sensor.get(oid, iid, 'sensorType');
+    sensor.read(oid, iid, rid, function (err, data) {
+    if (!err)
+        QIoT.qiotmqtt.type(sensortype,data,Qclient);
+    });
+}
 
-sensor.read('aIn', 0, 'aInCurrValue', function (err, data) {
-    if (!err)
-        console.log('S:' + data);  // 18.4 
-});
-sensor.read('aIn', 1, 'aInCurrValue', function (err, data) {
-    if (!err)
-        console.log('R:' + data);  // 18.4 
-});
-sensor.read('aIn', 2, 'aInCurrValue', function (err, data) {
-    if (!err)
-        console.log('P:' + data);  // 18.4 
-});
-sensor.read('aIn', 3, 'aInCurrValue', function (err, data) {
-    if (!err)
-        console.log('L:' + data);  // 18.4 
-});
+function teansdata(){
+
+    //senddata('temperature', 0, 'sensorValue');
+    senddata('aIn', 0, 'aInCurrValue');
+    senddata('aIn', 1, 'aInCurrValue');
+    senddata('aIn', 2, 'aInCurrValue');
+    senddata('aIn', 3, 'aInCurrValue');
+
+    senddata('dIn', 0, 'dInState');
+    senddata('dIn', 1, 'dInState');
+
+    setTimeout(function() {
+        console.log("wating......");
+        teansdata();
+    }, 100);
+}
+initsensor();
+teansdata();
